c3a178522ee8370858f59ad3c8eb5357e864c573
Jacob Peters <jacob.peters@aol.com>
2021-02-02 13:26:09 -0500

    turning Calendar from a function to a component

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,20 +1,27 @@
 import DayJS from 'dayjs';
+import React from 'react';
 
-function Calendar() {
-  const monthList = (
-    <div>
-      <h1>{DayJS().month(0).format('MMM').toString()}</h1>
-      <ul>
-        <li>{DayJS().month(0).daysInMonth()}</li>
-      </ul>
-    </div>
-  );
 
-  return (
-    <div className="Calendar">
-      {monthList}
-    </div>
-  )
+class Calendar extends React.Component {
+  render() {
+    let monthList = DayJS().month(0).format('MMM').toString()
+
+    let days = []
+    for (let i=1; i <= parseInt(DayJS().month(0).daysInMonth()); i++) {
+      days.push(i);
+    }
+  
+    let daysList = days.map((day, i) =>
+      <li key={DayJS().month(0).format('MMM').toString()+'_day'}>{day}</li>
+    )
+  
+    return (
+      <div className="Calendar">
+        <h1>{monthList}</h1>
+        <ul>{daysList}</ul>
+      </div>
+    )
+  }
 }
 
 export default Calendar;
