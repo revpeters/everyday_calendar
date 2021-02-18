d22c96a3f4e7304058137e6e8a6b854faab30ddf
Jacob Peters <jacob.peters@aol.com>
2021-02-17 22:34:11 -0500

    got it rendering pretty close to how the vanilla JS version did. i can do better with the constructor and functions

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,27 +1,68 @@
 import DayJS from 'dayjs';
+//import { months } from 'dayjs/locale/*';
 import React from 'react';
 
 
 class Calendar extends React.Component {
-  render() {
-    let monthList = DayJS().month(0).format('MMM').toString()
+  constructor(props) {
+    super(props);
 
-    let days = []
-    for (let i=1; i <= parseInt(DayJS().month(0).daysInMonth()); i++) {
-      days.push(i);
+    let getCalendar = (months) => {
+      let calendarYear = []
+      for (const month in months) {
+        let monthArray = []
+        for (let i = 1; i <= parseInt(DayJS().month(month).daysInMonth()); i++) {
+          monthArray.push(i);
+        }
+        //let monthName = DayJS().month(month).format('MMM').toString()
+        calendarYear.push(monthArray)
+      }
+      return calendarYear
     }
-  
-    let daysList = days.map((day, i) =>
-      <li key={DayJS().month(0).format('MMM').toString()+'_day'}>{day}</li>
-    )
-  
+    const testDays = getCalendar([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
+    this.state = {
+      fullYear: [...testDays]
+    }
+  }
+  render() {
     return (
       <div className="Calendar">
-        <h1>{monthList}</h1>
-        <ul>{daysList}</ul>
+        {this.state.fullYear.map((month, index) => {
+          return (
+            <div>
+              {DayJS().month(index).format('MMM').toString()}
+              <ul>
+                {month.map(day => {
+                  return (
+                    <li key={day + Math.random()}>
+                      {day}
+                    </li>
+                  )
+                })}
+
+              </ul>
+            </div>
+          )
+        })}
       </div>
     )
   }
 }
 
 export default Calendar;
+
+/*
+let getCalendar = (months) => {
+  let calendarYear = []
+  for (const month in months) {
+    let monthArray = []
+    for (let i=1; i <= parseInt(DayJS().month(month).daysInMonth()); i++) {
+      monthArray.push(i);
+    }
+    calendarYear.push(monthArray)
+  }
+  return calendarYear
+}
+const testDays = getCalendar([0,1,2,3,4,5,6,7,8,9,10,11])
+console.log(testDays) */
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Calendar from './Calendar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
     <Calendar />
   </React.StrictMode>,
   document.getElementById('root')
